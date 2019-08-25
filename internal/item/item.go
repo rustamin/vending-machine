101a3eece7c2ec66dd80db0623fc4c580f8c50ae
rustamin <rustamin@payfazz.com>
2019-08-25 17:13:01 +0700

    validate balance before update choose item

--- internal/item/item.go
+++ internal/item/item.go
@@ -1,6 +1,7 @@
 package item
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/rustamin/vending-machine/model"
@@ -19,12 +20,19 @@ func List(chooseItem []model.Item) {
 	fmt.Println(list)
 }
 
-func UpdateChooseItem(chooseItem []model.Item, goods []*model.Good, input int) []model.Item {
+func UpdateChooseItem(chooseItem []model.Item, goods []*model.Good, input int, balance *int) ([]model.Item, error) {
+
 	item := new(model.Item)
 
 	for i, elem := range goods {
 
 		if i == input-1 {
+			// TODO: validate if good is not found
+			if *balance-elem.Price < 0 {
+				return chooseItem, errors.New("Balance is not enough. Insert more coins")
+			}
+
+			// *balance = *balance - elem.Price
 			chooseItem = append(chooseItem, model.Item{
 				Name:  elem.Name,
 				Price: elem.Price,
@@ -34,5 +42,5 @@ func UpdateChooseItem(chooseItem []model.Item, goods []*model.Good, input int) [
 			item.Price = elem.Price
 		}
 	}
-	return chooseItem
+	return chooseItem, nil
 }
